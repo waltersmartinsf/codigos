823a744434c581b1829e0e0d67f33a3359def6c7
waltersmartinsf <waltersmartinsf@gmail.com>
2014-02-05 12:44:46 -0200

    Update clean.sh
    O clean.sh limpa o chache de memória. O cache de memória do sistema salva as informações de programas que acabaram de rodar, alocando memória que possa ser útil para outro programa. Isto é feito de forma automática, para garantir que se o código/programa que esteja rodando pare de funcionar seja retomado, porém, se ele usa muito da memória da máquina, é possível que o próprio sistema fique comprometido, e não tenha memória RAM suficiente para comandos e programas posteriores. A limpeza de cache é uma forma de liberar esta memória, retomando o sistema no estado anterior ao uso desta memória. 
    
    Exemplo: rodando máquina virtual de testes
    
    O uso de memória na máquina virtual possui algumas customizações, como quanto será usado para vídeo, e memória RAM. Porém, ele não é limitado com a memória de cache, e quanto mais tempo de uso, mais lento o sistema hospedeiro torna-se.
    
    Exemplo: Simulação em hidrocódigos: interior estelar, e planetário, n-corpos.
    
    Dependendo do código, não existe limitação e liberação de memória usada. A máquina pode ficar sobrecarregada. Usar este script shell pode ser uma alternativa, dentro das rotinas, para liberar memória do cache a cada passo. Neste caso, é importante conhecer como o programa/código interage com a máquina.

--- clean.sh
+++ clean.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # use as root
+#limpeza da memoria de cache so sitema
+
+#item 01: pagecache: limpeza da memoria de paginas
 sync && echo 1 > /proc/sys/vm/drop_caches
+
+#item 02: liberacao dos dentries e dos inodes do sistema
 sync && echo 2 > /proc/sys/vm/drop_caches
+
+#item 03: recorrencia, limpeza do pagecache, dentries, e inodes 
 sync && echo 3 > /proc/sys/vm/drop_caches
